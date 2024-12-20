0aedf7e9f3ebb98e2387d82ecea177a5de936a2d
mainabhihun <youmailmejust123@gmail.com>
2024-12-20 14:40:11 +0530

    removed postgres , working with redis

--- src/index.ts
+++ src/index.ts
@@ -13,19 +13,23 @@ app.post("/createshortlinks", async (req, res) => {
     const {
       shortenedUrl,
       originalUrl,
-    }: { shortenedUrl: string; originalUrl: string } = req.body;
-    //get the unique thing form shortened url(localhost:3000/dub/12345678)
-    console.log(req.body);
+    }: { shortenedUrl: string; originalUrl: any } = req.body;
+
+    console.log(req.body, "received from the body"); //
+
     //set to the redis and then in get link fetch from the redis
-    redisClient.hSet(`urls:${shortenedUrl}`, {
+    await redisClient.hSet(`urls:/dub/${shortenedUrl}`, {
       link: originalUrl,
       clicks: 0,
     });
-    const url = await prisma.url.create({
-      data: { shortenedUrl: shortenedUrl, originalUrl: originalUrl },
+    // const url = await prisma.url.create({
+    //   data: { shortenedUrl: shortenedUrl, originalUrl: originalUrl },
+    // });
+    // console.log(url);
+    res.status(200).json({
+      message: "link shortened",
+      shortenedUrl: `localhost:5173/dub/${shortenedUrl}`,
     });
-    console.log(url);
-    res.status(200).json({ message: "link shortend" });
   } catch (error: any) {
     console.log(error.message);
     res.status(500).json({ message: "server error" });
@@ -35,26 +39,29 @@ app.post("/createshortlinks", async (req, res) => {
 app.post("/getoriginallink", async (req, res) => {
   try {
     const { shortenedUrl } = req.body;
-    // prisma.$transaction({}) - to avoid use the race condition(multiple requests) use the atomic transaction
-    //retrieving from the redis
-    // redisClient.hGet()
-    const urlobject = await prisma.url.findFirst({
-      where: { shortenedUrl: shortenedUrl },
-    });
+    console.log(shortenedUrl, "fafsffsffdsfsd"); //check the data received
+    // //redis logic to get the original link
+    const originalUrl = await redisClient.hGet(`urls:${shortenedUrl}`, "link");
+    await redisClient.hIncrBy(`url:${shortenedUrl}`, "clicks", 1);
+    console.log(originalUrl);
+    //commented out the prisma logic
+    // const urlobject = await prisma.url.findFirst({
+    //   where: { shortenedUrl: shortenedUrl },
+    // });
     //increase the counter
-    if (urlobject) {
-      await prisma.url.update({
-        where: { id: urlobject.id },
-        data: {
-          counter: {
-            increment: 1, //look at this syntax we can use increment
-          },
-        },
-      });
-    }
-
-    console.log(shortenedUrl, urlobject);
-    res.status(200).json({ originalUrl: urlobject?.originalUrl });
+    // if (urlobject) {
+    //   await prisma.url.update({
+    //     where: { id: urlobject.id },
+    //     data: {
+    //       counter: {
+    //         increment: 1, //
+    //       },
+    //     },
+    //   });
+    //   console.log(shortenedUrl, urlobject);
+    // }
+    // res.status(200).json({ originalUrl: urlobject?.originalUrl });
+    res.status(200).json({ originalUrl: originalUrl });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "server error encountered" });
